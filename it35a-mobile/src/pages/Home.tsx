eb1caeafacb3bb84409eb0ddc7d7b15a8339831d
bearbrand-sy <sanderperejan@gmail.com>
2026-04-05 21:11:18 +0300

    added inital menu

--- it35a-mobile/src/pages/Home.tsx
+++ it35a-mobile/src/pages/Home.tsx
@@ -8,6 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
